66cef66fc2d431760800bb6879327814fa6a1e6c
Tomasz <estewez@gmail.com>
2017-12-02 15:11:29 +0100

    task completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-var maleNames = ['Krzysztof', 'Ignacy', 'Wiktor'];
-var femaleNames = ['Maria', 'Stefania', 'Katarzyna'];
-var allNames = maleNames.concat(femaleNames);
-var newName1 = 'Marian';
-var newName2 = 'Maria';
-if (allNames.indexOf(newName1) === -1) {
-    allNames.push(newName1); 
-}
-if (allNames.indexOf(newName2) === -1) {
-    allNames.push(newName2); 
-}
-console.log(allNames);
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+dinosaur = dinosaur.toUpperCase();
+text = text.replace('Velociraptor', dinosaur);
+console.log(text.substr(0, text.length/2));
